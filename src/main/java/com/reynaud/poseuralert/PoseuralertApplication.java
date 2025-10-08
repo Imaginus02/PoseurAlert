5140283c754c514c85f25de81afb9aa9e22467a1
Imaginus02 <tom.reynaud@etu.emse.fr>
2025-10-08 15:44:01 +0200

    Project start

---
+++ src/main/java/com/reynaud/poseuralert/PoseuralertApplication.java
@@ -0,0 +1,13 @@
+package com.reynaud.poseuralert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseuralertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoseuralertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reynaud/poseuralert/PoseuralertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reynaud.poseuralert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseuralertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
